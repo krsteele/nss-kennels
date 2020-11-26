5d1b7bc29ba88359d8cae49671201dbaab1deb73
Kristen Steele <ksteelephoto@gmail.com>
2020-11-25 19:26:36 -0600

    add all components necessary to render nav bar to DOM and route application views

---
+++ src/components/ApplicationViews.js
@@ -0,0 +1,44 @@
+import React from "react"
+import { Route } from "react-router-dom"
+import { LocationProvider } from "./location/LocationProvider"
+import { AnimalProvider } from "./animal/AnimalProvider"
+import { CustomerProvider } from "./customer/CustomerProvider"
+import { EmployeeProvider } from "./employee/EmployeeProvider"
+import { LocationList } from "./location/LocationList"
+import { AnimalList } from "./animal/AnimalList"
+import { CustomerList } from "./customer/CustomerList"
+import { EmployeeList } from "./employee/EmployeeList"
+
+export const ApplicationViews = (props) => {
+    return (
+        <>
+            <LocationProvider>
+                {/* Render the location list when http://localhost:3000/ */}
+                <Route exact path="/">
+                    <LocationList />
+                </Route>
+            </LocationProvider>
+
+            <AnimalProvider>
+                {/* Render the animal list when http://localhost:3000/animals */}
+                <Route path="/animals">
+                    <AnimalList />
+                </Route>
+            </AnimalProvider>
+
+            <CustomerProvider>
+                {/* Render the customer list when http://localhost:3000/customers */}
+                <Route path="/customers">
+                    <CustomerList />
+                </Route>
+            </CustomerProvider>
+
+            <EmployeeProvider>
+                {/* Render the employee list when http://localhost:3000/employees */}
+                <Route path="/employees">
+                    <EmployeeList />
+                </Route>
+            </EmployeeProvider>
+        </>
+    )
+}
--- src/components/Kennel.js
+++ src/components/Kennel.js
@@ -1,4 +1,7 @@
 import React from "react"
+import { Route } from "react-router-dom"
+import { NavBar } from "./nav/NavBar"
+import { ApplicationViews } from "./ApplicationViews"
 import "./Kennel.css"
 // import { Animal } from "./animal/Animal"
 import { AnimalProvider } from "./animal/AnimalProvider"
@@ -27,24 +30,7 @@ export const Kennel = () => (
             <div>500 Puppy Way</div>
         </address>
 
-        <h2>Animals</h2>
-        <AnimalProvider>
-            <AnimalList />
-        </AnimalProvider>
-        
-        <h2>Employees</h2>
-        <EmployeeProvider>
-            <EmployeeList />
-        </EmployeeProvider>
-        
-        <h2>Locations</h2>
-        <LocationProvider>
-            <LocationList />
-        </LocationProvider>
-
-        <h2>Customers</h2>
-        <CustomerProvider>
-            <CustomerList />
-        </CustomerProvider>
+        <NavBar />
+        <ApplicationViews />
     </>
 )
